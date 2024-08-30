64e8a7ca87b21fadb2d5722fb0e48810ca7ce24f
Amineelj11 <amine.elj@esprit.tn>
2024-08-30 14:11:50 +0100

    templates

---
+++ tours-and-travels-backend/src/main/java/com/toursandtravel/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.toursandtravel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // This corresponds to the home.html file
+    }
+}
